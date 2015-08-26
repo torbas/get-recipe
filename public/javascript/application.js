bc42ae8153232a25e39e40e07134db271ea40673
Timothy Orbasido <t.orbasido@gmail.com>
2015-08-26 22:54:00 +0000

    attempt to use food2fork api

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,21 @@
-$(document).ready(function() {
+$(function() {
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  request = $.ajax({
+        method: "get",
+        crossDomain: true,
+        contentType: "application/json; charset=utf-8",
+        jsonpCallback: 'callback',
+        dataType: "jsonp",
+        data: {
+          "key" : "9bd95bea53f5b286b7ffbaa71fb5cbe5",
+          "q"   : "chicken"
+        },
+        url: "http://food2fork.com/api/search"
+  })
+  .done(function(data){
+    $("<body>").append(data);
+  })
+  .fail(function(){
+    $("<body>").append("Something went wrong.");
+  });
 });
