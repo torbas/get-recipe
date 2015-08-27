8735d199d61ccf53cef0db9fdbdc9c620d5cd3c2
Timothy Orbasido <t.orbasido@gmail.com>
2015-08-27 00:47:46 +0000

    require net to attempt sinatra proxy

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,8 @@
 get '/' do
   erb :index
 end
+
+get '/recipes' do
+  uri = URI.parse('http://food2fork.com/api/search?key=9bd95bea53f5b286b7ffbaa71fb5cbe5&q=shredded%20chicken')
+  Net::HTTP.get(uri).to_json
+end
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -2,18 +2,10 @@ $(function() {
 
   request = $.ajax({
         method: "get",
-        crossDomain: true,
-        contentType: "application/json; charset=utf-8",
-        jsonpCallback: 'callback',
-        dataType: "jsonp",
-        data: {
-          "key" : "9bd95bea53f5b286b7ffbaa71fb5cbe5",
-          "q"   : "chicken"
-        },
-        url: "http://food2fork.com/api/search"
+        url: "/recipes"
   })
-  .done(function(data){
-    $("<body>").append(data);
+  .done(function(result){
+    $("<body>").append(result);
   })
   .fail(function(){
     $("<body>").append("Something went wrong.");
