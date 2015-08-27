a0c2d4134c010f62b328f129c5c6da441c585046
Timothy Orbasido <t.orbasido@gmail.com>
2015-08-27 02:58:56 +0000

    toggle showing ingredients

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -4,6 +4,9 @@ $(function() {
 
   var handlers = {
     loadSearch: function(data){
+      
+      recipesDiv.empty();
+
       request = $.ajax({
             method: "get",
             data: data,
@@ -27,13 +30,13 @@ $(function() {
                     }).append(recipeTitle);
 
             var showBtn = $("<div>",{
-                      "class": "btn btn-info show",
+                      "class": "btn btn-info toggle",
                       "text" : "Show Recipe",
                       "rid"  : recipe.recipe_id,
                     })
 
             var panelContent = $("<div>",{
-                      "class": "panel-body",
+                      "class": "panel-body hide",
                     });
 
             var assembled = panel.append(panelTitle).append(panelContent).append(showBtn);
@@ -44,6 +47,7 @@ $(function() {
     },
 
     loadRecipe: function(rid, targetElement){
+
       request = $.ajax({
             method: "get",
             url: "/recipe/"+rid,
@@ -52,10 +56,12 @@ $(function() {
       .done(function(result){
 
         var parsedRecipe = $.parseJSON(result);
+        var wrapper = $("<div>");
         parsedRecipe.recipe.ingredients.forEach(function(ingredient){
           var ingredientDiv = $("<div>", {"class": "ingredient"}).text(ingredient);
-          targetElement.prepend(ingredientDiv);
+          wrapper.append(ingredientDiv);
         });
+        targetElement.html(wrapper);
     
       });     
 
@@ -74,10 +80,19 @@ $(function() {
     return false;
   });
 
-recipesDiv.on("click", ".show", function(){
+  recipesDiv.on("click", ".toggle", function(){
+    var panelBody = $(this).siblings(".panel-body");
 
-  handlers.loadRecipe($(this).attr("rid"), $(this).sibling(".panel-body"));
+    if(panelBody.hasClass("hide"))
+      $(this).text("Hide Recipe");
+    else
+      $(this).text("Show Recipe");
 
-})
+    panelBody.toggleClass("hide");
+
+    if(!panelBody.children().length)
+      handlers.loadRecipe($(this).attr("rid"), panelBody);
+
+  });
 
 });
