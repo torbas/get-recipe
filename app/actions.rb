d36d61367f41056bf9fc030eb86e03d505f99611
Timothy Orbasido <t.orbasido@gmail.com>
2015-08-27 02:02:07 +0000

    implement search

--- app/actions.rb
+++ app/actions.rb
@@ -4,6 +4,13 @@ get '/' do
 end
 
 get '/recipes' do
-  uri = URI.parse('http://food2fork.com/api/search?key=9bd95bea53f5b286b7ffbaa71fb5cbe5&q=shredded%20chicken')
+  url = URI.escape("http://food2fork.com/api/search?key=9bd95bea53f5b286b7ffbaa71fb5cbe5&q=#{params[:query]}&count=10")
+  uri = URI.parse(url)
+  Net::HTTP.get(uri).to_json
+end
+
+get '/recipe/:id' do
+  url = URI.escape("http://food2fork.com/api/get?key=9bd95bea53f5b286b7ffbaa71fb5cbe5&rid=#{params[:id]}")
+  uri = URI.parse(url)
   Net::HTTP.get(uri).to_json
 end
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,13 +1,59 @@
 $(function() {
 
-  request = $.ajax({
-        method: "get",
-        url: "/recipes"
-  })
-  .done(function(result){
-    $("<body>").append(result);
-  })
-  .fail(function(){
-    $("<body>").append("Something went wrong.");
+  var recipesDiv = $("#recipes");
+  
+  var handlers = {
+    loadSearch: function(data){
+        request = $.ajax({
+              method: "get",
+              url: "/recipes",
+              dataType: "json"
+        })
+        .done(function(result){
+
+          var parsedSearch = $.parseJSON(result);
+
+          parsedSearch.recipes.forEach(function(recipe){
+
+              var recipeTitle = $("<h3>").text(recipe.title+" by "+recipe.publisher);
+
+              var panel = $("<div>",{
+                        "class": "panel panel-primary"
+                      });
+
+              var panelTitle = $("<div>",{
+                        "class": "panel-heading",
+                      }).append(recipeTitle);
+
+              var showBtn = $("<div>",{
+                        "class": "btn btn-info show",
+                        "text" : "Show Recipe",
+                        "rid"  : recipe.recipe_id,
+                      })
+
+              var panelContent = $("<div>",{
+                        "class": "panel-body",
+                      }).append(showBtn);
+
+              var assembled = panel.append(panelTitle).append(panelContent);
+
+              recipesDiv.append(assembled);
+          });
+        });
+    }
+  }
+
+  $("#search-form").on("submit", function(){
+    var query = $("#search").val().replace(" ",",");
+    var data = {
+      query:        query, 
+    }
+
+    handlers.loadSearch(data);
+
+    return false;
   });
+
+
+
 });
